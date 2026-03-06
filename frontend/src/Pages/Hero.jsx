ad966affa0bf3d2d80808bf767f1d2f2fff7a822
Muhammed Kasim TP <tpkasimvlr@gmail.com>
2026-03-07 00:18:56 +0530

    uppdate

--- frontend/src/Pages/Hero.jsx
+++ frontend/src/Pages/Hero.jsx
@@ -1,35 +1,35 @@
 import React from "react";
-// import hero from "../assets/hero.jpg";
+import hero from "../assets/hero.jpg";
 
 const Hero = () => {
   return (
     <section
-      className="relative w-full  h-full  flex items-center"
-      // style={{ backgroundImage: `url(${hero})` }}
+      className="relative w-screen min-h-screen flex items-center bg-cover bg-center"
+      style={{ backgroundImage: `url(${hero})` }}
     >
-      {/* Gradient Overlay */}
-      <div className="absolute inset-0 bg-gradient-to-r from-black/70 via-black/40 to-transparent"></div>
+      {/* Dark overlay */}
+      {/* <div className="absolute inset-0 bg-black/40"></div> */}
 
       {/* Content */}
-      <div className="relative max-w-7xl  bg-red-900  mx-auto px-6 w-full ">
-        <div className="max-w-xl backdrop-blur-md bg-white/10 p-8 rounded-2xl shadow-xl text-white">
+      <div className="relative max-w-7xl mx-auto px-6 w-full">
+        <div className="max-w-xl ">
 
-          <p className="text-4xl  md:text-5xl lg:text-6xl  font-bold leading-tight">
+          <p className="text-4xl  text-gray-700 sm:text-5xl md:text-6xl lg:text-7xl font-bold leading-tight">
             From the Blue Hills <br /> to Your Cup
           </p>
 
-          <p className="mt-5 text-lg md:text-xl text-gray-200">
+          <p className="mt-5 text-base sm:text-lg md:text-xl text-gray-600">
             Experience the true taste of Nilgiris Zyora Tea. Handpicked Nilgiri
             tea, freshly packed and delivered straight to your doorstep
             anywhere in India.
           </p>
 
-          <div className="mt-6 flex flex-wrap gap-4">
-            <button className="bg-green-600 hover:bg-green-700 px-7 py-3 rounded-lg font-semibold transition">
+          <div className="mt-8 flex flex-wrap gap-4">
+            <button className="bg-green-600 hover:bg-green-700 px-6 py-3 rounded-lg font-semibold transition">
               Order Now
             </button>
 
-            <button className="border border-white px-7 py-3 rounded-lg hover:bg-white hover:text-black transition">
+            <button className="border border-white px-6 py-3 rounded-lg hover:bg-white hover:text-black transition">
               Explore Tea
             </button>
           </div>
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -3,8 +3,8 @@ import Hero from '../Pages/Hero.jsx'
 
 const Home = () => {
   return (
-   <div className="max-w-7xl mx-auto">
-      <Hero />
+   <div className="max-w-7xl bg-red-000  ml-0 mx-auto">
+      <Hero  />
    </div>
   )
 }
